db4507aff22a2602d680ba99829142d4b80fd469
quantvhq999 <truongvanhongquan248@gmail.com>
2021-10-20 12:12:10 +0700

    Build source

--- src/component/layout/LayoutHome.js
+++ src/component/layout/LayoutHome.js
@@ -1,15 +1,31 @@
 import React from 'react'
 import { Row, Col, Button } from "antd";
-
 export default function LayoutHome(props) {
     return (
-        <div>
-            <Row>
-                <Col span={8}>
-                    <Button type="primary">A</Button>
+        <div style={{ height: "100vh" }}>
+            <Row className='container'>
+                <Col span={8} className='container__left'>
+                    <div className='container__left__head'></div>
+                    <div className='container__left__body'>
+                        <p className='container__left__body--slogan'>
+                            Đoàn kết - Chính trực<br />Tôn trọng - Chất lượng - Khát vọng vươn lên
+                        </p>
+                        <p className="container__left__body--description">
+                            Năng động, cập nhật kiến thức và áp dụng sáng tạo trong công việc.
+                        </p>
+                        <p className="container__left__body--description">
+                            Có đạo đức, lương tâm nghề nghiệp, ý thức kỷ luật và thái độ phục vụ tốt.
+                        </p>
+                        <Button type="primary" ghost="true" size="large">Đăng nhập</Button>
+                    </div>
+                    <div className='container__left__footer'>
+
+                    </div>
                 </Col>
-                <Col span={16}>
-                    <Button type="primary">A</Button>
+                <Col span={16} >
+                    <div>sad</div>
+                    <div>sad</div>
+                    <div>sad</div>
                 </Col>
             </Row>
         </div>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import "antd/dist/antd.css";
 import "../styles/antd.less";
+import "../styles/style.scss"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
