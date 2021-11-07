47758fed252e99f88c018aa213c7b4f493b8fdc2
quantvhq999 <truongvanhongquan248@gmail.com>
2021-11-08 00:21:39 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 const withPlugins = require('next-compose-plugins');
 const withAntdLess = require('next-plugin-antd-less');
+
+const babel = require('')
 const pluginAntdLess = withAntdLess({
   lessVarsFilePath: './src/styles/antd.less',
   modifyVars: './src/styles/antd.less'
 });
-module.exports = withPlugins([[pluginAntdLess]], {
+module.exports = withPlugins([], {
   webpack(config) {
     return config;
   },
