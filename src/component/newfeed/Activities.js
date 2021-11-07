91e6761ed383b668dec83b3b54dbfb29b3a0229f
quantvhq999 <truongvanhongquan248@gmail.com>
2021-11-08 02:09:51 +0700

    update

--- src/component/newfeed/Activities.js
+++ src/component/newfeed/Activities.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
 import { useDispatch, useSelector } from "react-redux";
 import { Skeleton } from 'antd';
 import ActivitiesItem from './ActivitiesItem';
