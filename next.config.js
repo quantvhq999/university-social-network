cde1cb53de3f9a07d623cf7551cc75d1b29b75be
quantvhq999 <truongvanhongquan248@gmail.com>
2021-11-08 01:33:50 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,15 +1,14 @@
 const withPlugins = require('next-compose-plugins');
 const withAntdLess = require('next-plugin-antd-less');
 const { default: Carousel } = require('react-multi-carousel');
-const withCarousel = require('react-multi-carousel')
 
 const pluginAntdLess = withAntdLess({
   lessVarsFilePath: './src/styles/antd.less',
   modifyVars: './src/styles/antd.less'
 });
 
-const pluginCarousel = withCarousel
-module.exports = withPlugins([[pluginAntdLess],new Carousel()], {
+const pluginCarousel = new Carousel()
+module.exports = withPlugins([[pluginAntdLess],[pluginCarousel]], {
   webpack(config) {
     return config;
   },
